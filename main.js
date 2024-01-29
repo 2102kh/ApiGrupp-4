efc627f0477b2f45cab137103a1d926b1f9d2d2a
Hanneslol <hannes@hanneswallin.com>
2024-01-29 13:59:10 +0100

    Added consts for all packets and added users
    Co-authored-by: 2102kh <2102kh@users.noreply.github.com>
    Co-authored-by: AnwarAsry <AnwarAsry@users.noreply.github.com>
    Co-authored-by: Christian Buaka <Christian.buaka@hotmail.com>

---
+++ main.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000 // "Radiofrekvens"
+
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(cors())
+
+
+const users = [
+    {
+        username: "Waw",
+        password: "waw123",
+        id: "324"
+    },
+    {
+        username: "Tag",
+        password: "tag123",
+        id: "321"
+    }
+]
+
+app.listen(port, (req, res) => {
+    res.send('Listening')
+})
+
+app.get('/api/users/:anvId', (req, res) => {
+    console.log(req.params.anvId)
+    let p = users.find(user => user.id == req.params.anvId)
+    // 404???
+    if (p == undefined) {
+        res.status(404).send('Finns inte')
+    }
+    res.json(p)
+});
